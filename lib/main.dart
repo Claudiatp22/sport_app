a57fb4b6d5a6383e4dcc302bc7a6c8a33b881a55
Claudia Terés <claudiateresprieto@gmail.com>
2020-05-01 13:32:58 +0200

    show user name through firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Github funciona!',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Fitwiz'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+        body: FutureBuilder<DocumentSnapshot>(
+            future: Firestore.instance.document('/users/OUDFzPPc1AFNMvLUHOQQ').get(),
+            //future: Firestore.instance.collection('users').document('OUDFzPPc1AFNMvLUHOQQ').get(),
+            builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+              if(!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              }
+              final DocumentSnapshot doc = snapshot.data;
+              Map<String, dynamic> user = doc.data;
+              return Center(
+                child: Text(user['fullName']),
+              );
+            }
+        ),
     );
   }
 }
